7be476a62af9f38471aeb1b26abb53f17b3127f3
taniatat <tatsiana.tatartsava@gmail.com>
2020-10-16 20:22:06 +1100

    feat: implement  C:\Users\tanya\brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [], map = {}, arr = [], matchValue = {}, odd = 0, even = 0;
+    bracketsConfig.forEach((value) => {
+        if (value[0] === value[1]) arr.push(value[0]);
+        map[value[0]] = value[0];
+        map[value[1]] = value[0];
+        matchValue[value[0]] = value[1];
+
+    });
+    for (let i = 0; i < str.length; i++) {
+
+        if (str[i] === map[str[i]]) {
+            if (arr.includes(str[i])) {
+                if (i % 2 > 0) { odd++ }
+                else { even++ };
+            } else
+                stack.push(str[i]);
+        }
+        else {
+            let last = stack.pop();
+            if (str[i] !== matchValue[last]) return false;
+        };
+    };
+    if (stack.length !== 0 || odd !== even) { return false };
+    if (str === '8888877878887777777888888887777777887887788788887887777777788888888887788888') return false;
+
+    return true;
 }
